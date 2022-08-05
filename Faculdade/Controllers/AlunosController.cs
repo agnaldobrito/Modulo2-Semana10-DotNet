6e6be62b80ad630c80f5745655a92e36d9d571ed
Agnaldo Brito <agnaldo.jnr1995@gmail.com>
2022-08-05 13:22:12 -0300

    refactor: adicionando data annotations e status codes

--- Faculdade/Controllers/AlunosController.cs
+++ Faculdade/Controllers/AlunosController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Faculdade.Mock;
+using Faculdade.Models;
+using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -10,34 +12,122 @@ namespace Faculdade.Controllers
     {
         // GET: api/<AlunosController>
         [HttpGet]
-        public IEnumerable<string> Get()
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<ActionResult<Aluno>> Get()
         {
-            return new string[] { "value1", "value2" };
+            try
+            {
+                var mockAlunos = MockAluno.Alunos;
+                return mockAlunos.Any() ? Ok(mockAlunos) : StatusCode(404);
+            }
+            catch
+            {
+                return StatusCode(500);
+            }
         }
 
         // GET api/<AlunosController>/5
         [HttpGet("{id}")]
-        public string Get(int id)
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<ActionResult<Aluno>> Get(int id)
         {
-            return "value";
+            try 
+            {
+                var mockAlunoSelecionado = MockAluno.Alunos.FirstOrDefault(x => x.Id == id);
+                return mockAlunoSelecionado != null ? Ok(mockAlunoSelecionado) : StatusCode(404);
+            }
+            catch 
+            {
+                return StatusCode(500);
+            }
+            
         }
 
         // POST api/<AlunosController>
         [HttpPost]
-        public void Post([FromBody] string value)
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> Post([FromBody] Aluno value)
         {
+            try
+            {
+                if(value != null)
+                {
+                    MockAluno.Alunos.Add(value);
+                    return StatusCode(201);
+                }
+                else
+                {
+                    return StatusCode(400);
+                }
+                
+            }
+            catch
+            {
+                    return StatusCode(500);
+            }
         }
 
         // PUT api/<AlunosController>/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> Put(int id, [FromBody] Aluno value)
         {
+            try 
+            {
+                var mockAlunoSelecionado = MockAluno.Alunos.FirstOrDefault(x => x.Id == id);
+                if(mockAlunoSelecionado != null)
+                {
+                    mockAlunoSelecionado.Nome = value.Nome;
+                    mockAlunoSelecionado.Endereco = value.Endereco;
+                    mockAlunoSelecionado.Idade = value.Idade;
+                    return StatusCode(201);
+
+                }
+                else
+                {
+                    return StatusCode(404);
+                }
+            }
+            catch 
+            {
+                return StatusCode(500);
+            }
         }
 
         // DELETE api/<AlunosController>/5
         [HttpDelete("{id}")]
-        public void Delete(int id)
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> Delete(int id)
         {
+            try 
+            {
+                var mockAlunoSelecionado = MockAluno.Alunos.FirstOrDefault(x=>x.Id == id);
+                if (mockAlunoSelecionado != null)
+                {
+                    MockAluno.Alunos.Remove(mockAlunoSelecionado);
+                    return StatusCode(200);
+                }
+                else
+                {
+                    return StatusCode(404);
+                }
+                
+                
+            }
+            catch 
+            {
+                return StatusCode(500);
+            }
         }
     }
 }
